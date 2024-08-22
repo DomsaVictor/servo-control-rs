58ffc0cedda6e6c34e54068e0474436714d3343f
DomsaVictor <domsavictor@gmail.com>
2024-08-22 21:45:19 +0300

    control angle in main loops

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::thread;
 
-use crossbeam_channel::bounded;
+use crossbeam_channel::{bounded, TrySendError};
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::ledc::{config::TimerConfig, LedcDriver, LedcTimerDriver, Resolution};
+use esp_idf_hal::task::current;
 use esp_idf_svc::hal::prelude::*;
 use log::{debug, info};
 
@@ -21,7 +22,11 @@ fn main() {
     let peripherals = Peripherals::take().unwrap();
 
     let (tx, rx) = bounded(1);
+    
+    info!("Sending 10 to angle!");
+    tx.send(10).unwrap();
 
+    
     let timer_driver = LedcTimerDriver::new(
         peripherals.ledc.timer0,
         &TimerConfig::default()
@@ -37,39 +42,114 @@ fn main() {
     )
     .unwrap();
 
-    // Get Max Duty and Calculate Upper and Lower Limits for Servo
-    let max_duty = driver.get_max_duty();
-    debug!("Max Duty {}", max_duty);
-    let min_limit = max_duty * 25 / 1000;
-    debug!("Min Limit {}", min_limit);
-    let max_limit = max_duty * 125 / 1000;
-    debug!("Max Limit {}", max_limit);
-
-    // Define Starting Position
-    driver
-        .set_duty(compute_duty(0, 0, 180, min_limit, max_limit))
-        .unwrap();
-    // Give servo some time to update
-    FreeRtos::delay_ms(500);
-
     let _servo_thread = std::thread::Builder::new()
         .stack_size(SERVO_STACK_SIZE)
         .spawn(move || servo_run_function(driver, rx));
-    
+
+
+    loop {
+        info!("Trying to send 100...");
+        match tx.try_send(100) {
+            Ok(_) => break,
+            Err(TrySendError::Full(_)) => {},
+            Err(TrySendError::Disconnected(_)) => {} 
+        }
+        FreeRtos::delay_ms(100);
+    }
+
+
+    loop {
+        info!("Trying to send 180...");
+        match tx.try_send(180) {
+            Ok(_) => break,
+            Err(TrySendError::Full(_)) => {},
+            Err(TrySendError::Disconnected(_)) => {} 
+        }
+        FreeRtos::delay_ms(100);
+    }
+
+    loop {
+        info!("Trying to send 0...");
+        match tx.try_send(0) {
+            Ok(_) => break,
+            Err(TrySendError::Full(_)) => {},
+            Err(TrySendError::Disconnected(_)) => {} 
+        }
+        FreeRtos::delay_ms(100);
+    }
+
+    let _ = _servo_thread.unwrap().join();
+
 }
 
 fn servo_run_function(
     mut driver: esp_idf_hal::ledc::LedcDriver,
-    rx: crossbeam_channel::Receiver<bool>,
+    rx: crossbeam_channel::Receiver<u32>,
 ) {
+    let mut current_angle: u32 = 0;
+
+    // Compute the min/max limits for the mapping funciton
+    // Get Max Duty and Calculate Upper and Lower Limits for Servo
+    let max_duty = driver.get_max_duty();
+    info!("Max Duty {}", max_duty);
+    let min_limit = max_duty * 25 / 1000;
+    info!("Min Limit {}", min_limit);
+    let max_limit = max_duty * 125 / 1000;
+    info!("Max Limit {}", max_limit);
+
+    let duty = compute_duty(current_angle, 0, 180, min_limit, max_limit);
+    info!("First Setting {duty}");
+
+    // Define Starting Position
+    driver
+        .set_duty(duty)
+        .unwrap();
+    // Give servo some time to update
+    FreeRtos::delay_ms(500);
+
+    info!("Moved to initial position");
+
+    let mut received = 0;
+
     loop {
-        driver
-            .set_duty(compute_duty(45, 0, 180, 409, 2048))
-            .unwrap();
-        FreeRtos::delay_ms(20);
+        match rx.try_recv() {
+            Ok(msg) => {
+                received = msg;
+                info!("Thread Received: {received}");
+            }
+            Err(_) => {}
+        }
+
+        info!("Received: {received} - Current: {current_angle}");
+
+        if current_angle < received {
+            while current_angle < received {
+                current_angle += 1;
+                let duty = compute_duty(current_angle, 0, 180, min_limit, max_limit);
+                info!("Current angle: {current_angle} - Setting {duty}");
+                driver
+                    .set_duty(duty)
+                    .unwrap();
+                FreeRtos::delay_ms(12);
+            }
+        }
+
+        if current_angle > received {
+            while current_angle > received {
+                current_angle -= 1;
+                let duty = compute_duty(current_angle, 0, 180, min_limit, max_limit);
+                info!("Current angle: {current_angle} - Setting {duty}");
+                driver
+                    .set_duty(duty)
+                    .unwrap();
+                FreeRtos::delay_ms(12);
+            }
+        }
+
+        FreeRtos::delay_ms(1000);
     }
 }
 
 fn compute_duty(angle: u32, in_min: u32, in_max: u32, min_limit: u32, max_limit: u32) -> u32 {
-    (angle - in_min) * (max_limit - min_limit) / (in_max - in_min) + max_limit
+    (angle - in_min) * (max_limit - min_limit) / (in_max - in_min) + min_limit
 }
